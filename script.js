3153e31a1e6c026fa20574d2cd0d726637351335
Kamilla Vitoria <117935629+myllavitoria@users.noreply.github.com>
2024-12-04 12:49:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script para adicionar funcionalidade simples no site (se necessário)
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada e pronta!");
+});
